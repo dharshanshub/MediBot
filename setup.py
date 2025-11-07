7209d54c747f14f6513b476726775fa9c9dbdea7
dharshanshub <dharshankr2000@gmail.com>
2025-11-07 17:58:09 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = "medibot",
+  version = "0.1.0",   
+  author="Dharshan",
+  author_email="dharshankr2000@gmail.com",
+  packages = find_packages( ),
+  install_requires = []
+)
